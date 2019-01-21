9f2630c3557db9954f5960ff8975f94549ef9a5e
mingz <305603665@qq.com>
2019-01-21 10:28:58 +0800

    隐藏导航栏

--- src/android/StatusBar.java
+++ src/android/StatusBar.java
@@ -119,7 +119,13 @@ public class StatusBar extends CordovaPlugin {
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
                         int uiOptions = window.getDecorView().getSystemUiVisibility()
                                 | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+                                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                                | View.SYSTEM_UI_FLAG_IMMERSIVE
+                                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+
 
                         window.getDecorView().setSystemUiVisibility(uiOptions);
                     }
